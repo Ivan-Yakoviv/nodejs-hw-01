5566a2b860910cf42335b31708410e1198bcbc90
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-09-08 02:11:45 +0300

    addOne, count, getAll, removeAll,removeLast

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,12 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const existingContacts = await readContacts();
+    return existingContacts;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 console.log(await getAllContacts());
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,19 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const existingContacts = await readContacts();
+    if (existingContacts.length > 0) {
+      existingContacts.pop();
+      await writeContacts(existingContacts);
+    } else {
+      console.log('No contacts to remove');
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 removeLastContact();
